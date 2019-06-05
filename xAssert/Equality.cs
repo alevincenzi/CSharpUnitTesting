d31a4e78deeafc8e2efd9be9d57accd08bad4c08
Alessandro Vincenzi <alessandro84.vincenzi@gmail.com>
2019-06-05 12:51:43 +0200

    Refactored assertions on equality

--- xAssert/Equality.cs
+++ xAssert/Equality.cs
@@ -6,28 +6,50 @@ namespace CSharpUnitTesting.xAssert
 {
     public class Equality
     {
+        // Warning xUnit2004
+        // --> Use Assert.True(expression)
+        // Assert.Equal<bool>(true, bActual);
+
+        // Warning xUnit2006
+        // --> Use Assert.Equal("x", "x")
+        // Assert.Equal<string>("x", "x");
+
+        // Warning xUnit2004
+        // --> Use Assert.True(expression)
+        // Assert.NotEqual<bool>(true, bActual);
+
+        // Warning xUnit2006
+        // --> Use Assert.NotEqual("x", "y")
+        // Assert.NotEqual<string>("x", "x");
+
         [Fact]
         public void Equal_Double()
         {
             Assert.Equal(0.3d, 0.1d + 0.2d, 1);
+            Assert.Throws<EqualException>(
+                () => Assert.Equal<double>(0.3d, 0.1d + 0.2d)
+            );
         }
 
         [Fact]
         public void NotEqual_Double()
         {
             Assert.NotEqual(0.3d, 0.1d + 0.2d);
+            Assert.NotEqual<double>(0.3d, 0.1d + 0.2d);
         }
 
         [Fact]
         public void Equal_Decimal()
         {
             Assert.Equal(0.30m, 0.10m + 0.20m, 0);
+            Assert.Equal<decimal>(0.30m, 0.3m);
         }
 
         [Fact]
         public void NotEqual_Decimal()
         {
             Assert.NotEqual(0.3m, 0.11m + 0.20m);
+            Assert.NotEqual<decimal>(0.30m, 0.31m);
         }
 
         [Fact]
@@ -36,110 +58,97 @@ namespace CSharpUnitTesting.xAssert
             Assert.Equal(
                 new DateTime(2000, 12, 25, 12, 30, 10),
                 new DateTime(2000, 12, 25, 12, 30, 11),
-                               new TimeSpan(0,  0,  1));
-        }
-
-        [Fact]
-        public void Equal_Type_Base()
-        {
-            Assert.Equal<int>(1, 1);
-            Assert.Equal<double>(1.0d, 1.0d);
-            Assert.Equal<char>('x', 'x');
-
-            // Warning xUnit2004
-            // --> Use Assert.True(expression)
-            // Assert.Equal<bool>(true, bActual);
-
-            // Warning xUnit2006
-            // --> Use Assert.Equal("x", "x")
-            // Assert.Equal<string>("x", "x");
-        }
-
-        [Fact]
-        public void NotEqual_Type_Base()
-        {
-            Assert.NotEqual<int>(0, 1);
-            Assert.NotEqual<double>(0.0d, 1.0d);
-            Assert.NotEqual<char>('x', 'y');
-
-            // Warning xUnit2004
-            // --> Use Assert.True(expression)
-            // Assert.NotEqual<bool>(true, bActual);
-
-            // Warning xUnit2006
-            // --> Use Assert.NotEqual("x", "y")
-            // Assert.NotEqual<string>("x", "x");
-        }
-
-        [Fact]
-        public void Equal_Type_Custom()
-        {
-            var ref1 = new AClass(1);
-            var ref2 = new AClass(1);
-            var ref3 = new AClassWithEquals(1);
-            var ref4 = new AClassWithEquals(1);
-            var ref5 = new AClassWithEquals(2);
-
-            Assert.Equal<AClass>(ref1, ref1);
-            
-            Assert.Throws<EqualException>(
-                () => Assert.Equal<AClass>(ref1, ref2)
+                                new TimeSpan(0,  0,  1)
             );
-            
-            Assert.Equal<AClassWithEquals>(ref3, ref4);
-
-            Assert.Throws<EqualException>(
-                () => Assert.Equal<AClassWithEquals>(ref4, ref5)
+            Assert.Equal<DateTime>(
+                new DateTime(2000, 12, 25, 12, 30, 10),
+                new DateTime(2000, 12, 25, 12, 30, 10)
             );
         }
 
         [Fact]
-        public void Equal_Type_Custom_WithEqualityComparer()
+        public void Equal_SameReference()
         {
-            var ref1 = new AClass(1);
-            var ref2 = new AClass(1);
-
-            Assert.Equal<AClass>(ref1, ref2, new AClassEqualityComparer());
+            var reference = new AClass(1);
+            Assert.Equal<AClass>(reference, reference);
         }
 
         [Fact]
-        public void Equal_Struct()
+        public void NotEqual_ThrowsException_WhenSameReference()
         {
-            AStruct ref1 = new AStruct(1, 2);
-            AStruct ref2 = new AStruct(1, 2);
-
-            Assert.Equal<AStruct>(ref1, ref1);
-            Assert.Equal<AStruct>(ref1, ref2);
+            var reference = new AClass(1);
+            Assert.Throws<NotEqualException>(
+                () => Assert.NotEqual<AClass>(reference, reference)
+            );
         }
 
         [Fact]
-        public void NotEqual_Type_Custom()
+        public void Equal_ClassWithoutEqual_ThrowsException()
         {
-            var ref1 = new AClass(1);
-            var ref2 = new AClass(1);
-            var ref3 = new AClassWithEquals(1);
-            var ref4 = new AClassWithEquals(2);
-
-            Assert.NotEqual<AClass>(ref1, ref2);
-            Assert.NotEqual<AClassWithEquals>(ref3, ref4);
+            Assert.Throws<EqualException>(
+                () => Assert.Equal<AClass>(new AClass(1), new AClass(1))
+            );
         }
 
         [Fact]
-        public void NotEqual_Type_Custom_WithEqualityComparer()
+        public void NotEqual_ClassWithoutEqual()
         {
-            var ref1 = new AClass(1);
-            var ref2 = new AClass(2);
-
-            Assert.NotEqual<AClass>(ref1, ref2, new AClassEqualityComparer());
+            Assert.NotEqual<AClass>(new AClass(1), new AClass(1));
         }
 
         [Fact]
-        public void NotEqual_Struct()
+        public void Equal_ClassWithEqual()
         {
-            AStruct ref1 = new AStruct(1, 2);
-            AStruct ref2 = new AStruct(3, 4);
+            Assert.Equal<AClassWithEquals>(
+                new AClassWithEquals(1), new AClassWithEquals(1)
+            );
+        }
 
-            Assert.NotEqual<AStruct>(ref1, ref2);
+        [Fact]
+        public void NotEqual_ClassWithEqual()
+        {
+            Assert.NotEqual<AClassWithEquals>(
+                new AClassWithEquals(1), new AClassWithEquals(2)
+            );
+        }
+
+        [Fact]
+        public void Equal_EqualityComparer()
+        {
+            Assert.Equal<AClass>(
+                new AClass(1),
+                new AClass(1),
+                new AClassEqualityComparer()
+            );
+        }
+
+        [Fact]
+        public void NotEqual_EqualityComparer()
+        {
+            Assert.NotEqual<AClass>(
+                new AClass(1),
+                new AClass(2),
+                new AClassEqualityComparer()
+            );
+        }
+
+        [Fact]
+        public void Equal_Struct_SameReference()
+        {
+            AStruct reference = new AStruct(1, 2);
+            Assert.Equal<AStruct>(reference, reference);
+        }
+
+        [Fact]
+        public void Equal_Struct_SameFields()
+        {
+            Assert.Equal<AStruct>(new AStruct(1, 2), new AStruct(1, 2));
+        }
+
+        [Fact]
+        public void NotEqual_Struct_DifferentFields()
+        {
+            Assert.NotEqual<AStruct>(new AStruct(1, 2), new AStruct(1, 3));
         }
     }
 }
